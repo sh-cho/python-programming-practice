5c577a2382e9715812a3df90f54431b708f4a99f
Seonghyeon Cho <seonghyeoncho96@gmail.com>
2021-10-22 19:38:31 +0900

    chore

--- client.py
+++ client.py
@@ -7,14 +7,8 @@ ADDR = (SERVER_HOST, PORT)
 FORMAT = "utf-8"
 DISCONNECT_MESSAGE = "!DISCONNECT"
 
-sock = socket.socket(
-    family=socket.AF_INET,
-    type=socket.SOCK_STREAM,
-)
-sock.connect(ADDR)
 
-
-def send(msg: str):
+def send(msg: str, sock: socket.socket):
     message = msg.encode(FORMAT)
     message_sz = len(message)
     padded = str(message_sz).ljust(BUF_SIZE, " ").encode(FORMAT)
@@ -22,4 +16,11 @@ def send(msg: str):
     sock.send(padded)
     sock.send(message)
 
-send("hello world!")
+
+if __name__ == "__main__":
+    sock = socket.socket(
+        family=socket.AF_INET,
+        type=socket.SOCK_STREAM,
+    )
+    sock.connect(ADDR)
+    send("hello world!", sock)
